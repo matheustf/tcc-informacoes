e524429238eecfa1b0c4585dd1ddfa64a96a9ec0
Matheus Teles <teles.matheus@hotmail.com>
2018-10-15 16:56:50 -0300

    init

---
+++ src/main/java/com/puc/tcc/informacoes/InformacoesApplication.java
@@ -0,0 +1,12 @@
+package com.puc.tcc.informacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacoesApplication.class, args);
+	}
+}
